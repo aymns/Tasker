9324372f03cb76c04127d2205f0f05fc46b292ad
Ayman <aymn.s@live.com>
2019-06-30 12:04:58 +0300

    Interface & Documentation

---
+++ Tasker/ITaskManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasker
+{
+    public interface ITaskManager : IDisposable
+    {
+        /// <summary>
+        /// Run the <param name="action"></param> in asynchronously separate thread if <see cref="TaskManager"/> is not not busy or wait
+        /// until there is a free thread.
+        /// </summary>
+        /// <param name="action"></param>
+        void DoWork(Action action);
+
+        /// <summary>
+        /// Cancel all unfinished tasks.
+        /// </summary>
+        void AbortAll();
+
+
+        /// <summary>
+        /// Get number of current running threads.
+        /// </summary>
+        int NumberOfRunningThreads { get; }
+    }
+}
--- Tasker/TaskManager.cs
+++ Tasker/TaskManager.cs
@@ -6,57 +6,65 @@ using System.Threading.Tasks;
 
 namespace Tasker
 {
-    public class TaskManager : IDisposable
+    /// <summary>
+    ///     Represent task manager used to control number of threads to execute async functions
+    /// </summary>
+    public class TaskManager : ITaskManager
     {
+        /// <summary>
+        ///     A delegate to be called when exception happened.
+        /// </summary>
+        /// <param name="e"></param>
         public delegate void HandleException(Exception e);
-
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-
+        private readonly Queue<Task> _currentRunningTasks = new Queue<Task>();
         private readonly HandleException _exceptionHandler;
         private readonly object _locker = new object();
-        private readonly int _maxThreads = 100;
+        public int MaxThreads { get; private set; } = 100;
         private readonly ManualResetEvent _resetEvent = new ManualResetEvent(true);
         private bool _abortAllCalled;
         private bool _isDisposed;
 
-        Queue<Task> _currentRunningTasks = new Queue<Task>();
-
+        /// <summary>
+        /// Initialize new instance of task manager, if <param name="maxThreads"></param> is 1 then the task manager will work in a sync way
+        /// means there will be one thread
+        /// </summary>
+        /// <param name="maxThreads">max number of threads the task manager can create.</param>
+        /// <param name="exceptionHandler">delegate function to be called when exception occured.</param>
         public TaskManager(int maxThreads, HandleException exceptionHandler)
         {
-            if (_maxThreads < 1)
+            if (MaxThreads < 1)
                 throw new InvalidOperationException("Number of max threads has to be greater than 1");
 
-            _maxThreads = maxThreads;
+            MaxThreads = maxThreads;
             _exceptionHandler = exceptionHandler;
         }
 
+        /// <summary>
+        /// Initialize new instance of task manager, if <param name="maxThreads"></param> is 1 then the task manager will work in a sync way
+        /// means there will be one thread
+        /// </summary>
+        /// <param name="maxThreads">max number of threads the task manager can create.</param>
         public TaskManager(int maxThreads) : this(maxThreads, null)
         {
         }
 
+
+        /// <inheritdoc />
         public int NumberOfRunningThreads { private set; get; }
 
         public void Dispose()
         {
             if (!_abortAllCalled)
-                WaitForTheLastTaskToFinish();
+                WaitForAllTasksToFinish();
 
             _isDisposed = true;
             _cancellationTokenSource.Cancel();
             _resetEvent.Dispose();
             AbortAll();
         }
-        
-        private void WaitForTheLastTaskToFinish()
-        {
-            if (!_currentRunningTasks.Any())
-            {
-                return;
-            }
 
-            Task.WaitAll(this._currentRunningTasks.ToArray());
-        }
-
+        /// <inheritdoc />
         public void DoWork(Action action)
         {
             if (action == null)
@@ -66,13 +74,13 @@ namespace Tasker
                 throw new InvalidOperationException(
                     "Cannot call DoWork() after AbortAll() or Dispose() have been called");
 
-            if (!_isDisposed && _maxThreads > 1)
+            if (!_isDisposed && MaxThreads > 1)
             {
                 _resetEvent.WaitOne();
                 lock (_locker)
                 {
                     NumberOfRunningThreads++;
-                    if (!_isDisposed && NumberOfRunningThreads >= _maxThreads)
+                    if (!_isDisposed && NumberOfRunningThreads >= MaxThreads)
                         _resetEvent.Reset();
                 }
 
@@ -85,6 +93,7 @@ namespace Tasker
             }
         }
 
+        /// <inheritdoc />
         public void AbortAll()
         {
             _abortAllCalled = true;
@@ -96,6 +105,26 @@ namespace Tasker
             _cancellationTokenSource.Cancel();
         }
 
+
+        /// <summary>
+        /// This method used before disposing task manager, more specifically to wait the last task to finish.
+        /// </summary>
+        private void WaitForAllTasksToFinish()
+        {
+            if (!_currentRunningTasks.Any())
+                return;
+
+            Task.WaitAll(_currentRunningTasks.ToArray());
+        }
+
+        /// <summary>
+        /// start <param name="action"></param>,
+        /// when it finishes do the following
+        ///     - If <see cref="decrementRunningThreadCountOnCompletion"/> decrease number of running threads <see cref="NumberOfRunningThreads"/>
+        ///     - If <see cref="CanCreateNewThread"/> is true then send a manual reset event, to release block on starting new threads
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="decrementRunningThreadCountOnCompletion"></param>
         private void RunAction(Action action, bool decrementRunningThreadCountOnCompletion = true)
         {
             try
@@ -107,14 +136,21 @@ namespace Tasker
                 if (decrementRunningThreadCountOnCompletion)
                     lock (_locker)
                     {
-                        this._currentRunningTasks.Dequeue();
+                        _currentRunningTasks.Dequeue();
                         NumberOfRunningThreads--;
-                        if (!_isDisposed && NumberOfRunningThreads < _maxThreads)
+                        if (CanCreateNewThread)
                             _resetEvent.Set();
                     }
             }
         }
 
+        private bool CanCreateNewThread => !_isDisposed && NumberOfRunningThreads < MaxThreads;
+
+        /// <summary>
+        /// Run action on new thread.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
         private Task RunActionOnDedicatedThread(Action action)
         {
             var task = Task.Factory
@@ -125,6 +161,10 @@ namespace Tasker
             return task;
         }
 
+        /// <summary>
+        /// Handle task exceptions
+        /// </summary>
+        /// <param name="task"></param>
         private void HandleAggregateExceptions(Task task)
         {
             if (task?.Exception == null || _exceptionHandler == null)
